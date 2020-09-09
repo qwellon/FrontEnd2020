dbbe723a594c412b86120c4ef4d6af10f2d9614a
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-09-09 21:19:45 +0300

    learn js operatori

--- tryjs/app.js
+++ tryjs/app.js
@@ -4,4 +4,30 @@ admin = name;
 alert(admin);
 let planetName;
 let currentUser;
+
+"" + 1 + 0 // 10 str	
+"" - 1 + 0 // NaN **-1
+true + false // true boolean **1
+6 / "3" // 2 num
+"2" * "3" // 6 num
+4 + 5 + "px" // 9px str
+"$" + 4 + 5 // $45 str
+"4" - 2 // 2 num
+"4px" - 2 // NaN
+7 / 0 // NaN **infinity
+"  -9  " + 5 //   -9  5 str
+"  -9  " - 5 // NaN ** -14
+null + 1 // 1 num
+undefined + 1 // NaN
+" \t \n" - 2 // NaN ** -2 
+
+
+let a = 1, b = 1;
+
+let c = ++a; // ? c=2 a=2
+let d = b++; // ? b=2 d=1
+
+let a = 2;
+
+let x = 1 + (a *= 2); x=5 a=4
  
