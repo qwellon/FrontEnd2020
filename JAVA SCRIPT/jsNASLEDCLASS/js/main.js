0a0b6012f2eb9e0f524ede5f4c1f746670a72464
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-10-28 21:13:27 +0300

    practice NASLED

--- JAVA SCRIPT/jsNASLEDCLASS/js/main.js
+++ JAVA SCRIPT/jsNASLEDCLASS/js/main.js
@@ -1,7 +1,7 @@
 //квадрат прямоуг треугольник
 //params array
 //name string
-class Figure {
+/*class Figure {
     constructor(name, params) {
         this.name = name;
         this.params = params;
@@ -32,4 +32,46 @@ let obj = new Square('Квадрат', [2, 2, 2, 2]);
 console.log(objSquare.getArea());
 console.log(objSquare.get());
 
-let objTriangle = new Triangle('Треугольник ', [1, 2, 3]);
+let objTriangle = new Triangle('Треугольник ', [1, 2, 3]);*/
+
+<html><head></head><body>
+    <form name="search">
+        <label>Search the site:
+      <input type="text" name="search">
+    </label>
+            <input type="submit" value="Search!">
+  </form>
+
+            <hr>
+
+                <form name="search-person">
+                    Search the visitors:
+    <table id="age-table">
+                        <tbody><tr>
+                            <td>Age:</td>
+                            <td id="age-list">
+                                <label>
+                                    <input type="radio" name="age" value="young">less than 18</label>
+                                    <label>
+                                        <input type="radio" name="age" value="mature">18-50</label>
+                                        <label>
+                                            <input type="radio" name="age" value="senior">more than 50</label>
+        </td>
+      </tr>
+
+                                    <tr>
+                                        <td>Additionally:</td>
+                                        <td>
+                                            <input type="text" name="info[0]">
+                                                <input type="text" name="info[1]">
+                                                    <input type="text" name="info[2]">
+        </td>
+      </tr>
+
+    </tbody></table>
+
+                                        <input type="submit" value="Search!">
+  </form>
+
+
+</body></html>
