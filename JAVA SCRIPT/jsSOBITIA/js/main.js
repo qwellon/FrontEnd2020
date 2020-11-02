6c53ab9a3007d70a9251989b95eb94c4499bbb4a
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-11-02 21:29:48 +0300

    jsEVENTS

---
+++ JAVA SCRIPT/jsSOBITIA/js/main.js
@@ -0,0 +1,11 @@
+
+
+function displayNone() {
+
+    let elem = document.querySelector('.click__form');
+    let elem2 = document.querySelector('.click__bg');
+    console.log(elem);
+    console.log(elem2);
+    elem.style.display = "none";
+    elem2.style.display = "none";
+}
