8f84a433e3958fa49e11713298a07da43bc76c7e
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-11-04 21:19:38 +0300

    jsSOBITIA practic

--- JAVA SCRIPT/jsSOBITIA/js/main.js
+++ JAVA SCRIPT/jsSOBITIA/js/main.js
@@ -16,3 +16,32 @@ function displayFlex() {
     elem.style.display = "flex";
     elem2.style.display = "flex";
 }
+
+function displayPromptPhone() {
+    let elem = document.querySelector('.form__prompt-phone');
+    let phone = document.querySelector('.click__phonenum');
+    //elem.style.visibility = "visible";
+    phone.addEventListener("focusin", () => elem.style.visibility = "visible");
+    phone.addEventListener("focusout", () => elem.style.visibility = "hidden");
+
+}
+
+function displayPromptPassword() {
+    let elem = document.querySelector('.form__prompt-password');
+    let pass = document.querySelector('.click__password');
+    //elem.style.visibility = "visible";
+    pass.addEventListener("focusin", () => elem.style.visibility = "visible");
+    pass.addEventListener("focusout", () => elem.style.visibility = "hidden");
+}
+
+function submitEvent() {
+
+    let phone = document.querySelector('.click__phonenum');
+    let pass = document.querySelector('.click__password');
+    let type = typeof phone;
+    console.log(phone.value);
+    console.log(type);
+    if (phone.value == false || phone.length >= 10 || type != "number") {
+        phone.style.background = "#f79eb0";
+    }
+}
