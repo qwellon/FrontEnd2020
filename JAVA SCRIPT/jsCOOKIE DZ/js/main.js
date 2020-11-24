8cb85e5ac573018cfb0c5e69f7278b4150c95e11
qwellon <ventanatul@gmail.com>
2020-11-24 20:47:54 +0300

    dz cookie

--- JAVA SCRIPT/jsCOOKIE DZ/js/main.js
+++ JAVA SCRIPT/jsCOOKIE DZ/js/main.js
@@ -1,12 +1,55 @@
 $(function () {
 
 });
-$( "#click" ).click(function() {
+$("#click").click(function () {
 
     let elem = document.querySelector('.codeName');
     let colname = document.querySelector('.colorName');
-    
-   $(".color_block").css('background-color', elem.value );
-   // $(".name").text(colorName.value);
-   $('.name').text(colname.value);
+
+    $(".color_block").css('background-color', elem.value);
+    // $(".name").text(colorName.value);
+    $('.name').text(colname.value);
 });
+
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/',
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let codeName = document.querySelector('.codeName');
+    let colorName = document.querySelector('.colorName');
+    let cookieCode = getCookie('codeName');
+    let cookieColor = getCookie('colorName');
+
+    codeName.value = (cookieCode === undefined) ? 0 : cookieCode;
+    colorName.value = (cookieColor === undefined) ? 0 : cookieColor;
+})
